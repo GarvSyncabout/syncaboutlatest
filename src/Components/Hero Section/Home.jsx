8c979bac82e2dd87131a965541644e8fdef85dab
GarvSyncabout <sbs.gs2024@gmail.com>
2024-11-26 19:00:14 +0530

    Hero Section Changes

--- src/Components/Hero Section/Home.jsx
+++ src/Components/Hero Section/Home.jsx
@@ -16,13 +16,6 @@ const Home = () => {
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   );
