ccdee8ab1097ceb74f9f943cf46d510a8c4b8d1d
GarvSyncabout <sbs.gs2024@gmail.com>
2024-12-09 18:22:50 +0530

    changes

--- src/Pages/AboutUs/Aboutus.jsx
+++ src/Pages/AboutUs/Aboutus.jsx
@@ -5,30 +5,38 @@ import "../AboutUs/aboutus.css";
 const Aboutus = () => {
   return (
     <section className="w-full  aboutus-bg bg-[url('/aboutus/aboutus.jpg')] bg-no-repeat bg-cover ">
-      <div
-        id="aboutus-wrapper"
-        className="gap-3 flex flex-wrap justify-start md:justify-between md:h-[80vh] items-center m-auto w-full max-w-7xl p-5"
-      >
-        <div>
-          <span className="font-Jost text-[#ffffff] uppercase text-lg leading-[30px] tracking-[5px] font-normal">
-            We are dedicated
-          </span>
-          <h2 className="font-Manrope text-5xl md:text-[80px] md:leading-[88px] font-extrabold text-[#ffffff]">
-            About Us
-          </h2>
-        </div>
-        <div>
-          <div className="bg-[#ef7f1a] p-6 md:py-[25px] md:px-[60px] rounded-full">
-            <ul className="flex items-center gap-5">
-              <li className="text-[#fff] text-base hover:text-[#454647] leading-[20px] font-Jost font-medium relative after:content-['\2192'] after:absolute  after:ml-1 after:text-white cursor-pointer">
-                <Link to={"/"}>Home</Link>
-              </li>
-              <li className="font-Jost text-base leading-5 font-medium text-[#ffffff] cursor-pointer">
-                About Us
-              </li>
-            </ul>
+      <div className="m-auto w-full max-w-7xl">
+        {/* About section hero page */}
+
+        <div
+          id="aboutus-wrapper"
+          className="gap-3 flex flex-wrap justify-start md:justify-between md:h-[80vh] items-center p-5"
+        >
+          <div>
+            <span className="font-Jost text-[#ffffff] uppercase text-lg leading-[30px] tracking-[5px] font-normal">
+              We are dedicated
+            </span>
+            <h2 className="font-Manrope text-5xl md:text-[80px] md:leading-[88px] font-extrabold text-[#ffffff]">
+              About Us
+            </h2>
+          </div>
+          <div>
+            <div className="bg-[#ef7f1a] p-4 md:py-[25px] md:px-[60px] rounded-full">
+              <ul className="flex items-center gap-5">
+                <li className="text-[#fff] text-base hover:text-[#454647] leading-[20px] font-Jost font-medium relative after:content-['\2192'] after:absolute  after:ml-1 after:text-white cursor-pointer">
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li className="font-Jost text-base leading-5 font-medium text-[#ffffff] cursor-pointer">
+                  About Us
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+
+        {/* main content */}
+
+        <div></div>
       </div>
       <div className="aboutus-content-bottom">
         <h1 className="font-Poppins text-4xl md:text-6xl  lg:text-8xl uppercase   lg:leading-[128px] font-bold opacity-20">
